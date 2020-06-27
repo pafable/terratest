6fa3ec2666540d9537a4b4d25257f626095b93c2
Philip Afable <philafable@gmail.com>
2020-06-27 11:33:59 -0400

    added basic

---
+++ my-container-test/docker_test.go
@@ -0,0 +1,23 @@
+package test
+
+import (
+	"testing"
+
+	"github.com/gruntwork-io/terratest/modules/terraform"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestTerraformDocker(t *testing.T) {
+	terraformOptions := &terraform.Options{
+		TerraformDir: "../my-container-test",
+	}
+
+	// executes terraform destroy after creating
+	defer terraform.Destroy(t, terraformOptions)
+
+	// executes terraform init and apply to check containers
+	terraform.InitAndApply(t, terraformOptions)
+
+	output := terraform.Output(t, terraformOptions, "resourceName")
+	assert.Equal(t, "Hello, world!", output)
+}
