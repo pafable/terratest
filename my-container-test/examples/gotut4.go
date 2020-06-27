fb51773f01bc35f322dadc38ebf8876e6d60cbe2
Philip Afable <philafable@gmail.com>
2020-06-26 22:29:14 -0400

    created car strut

---
+++ my-container-test/examples/gotut4.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+type cat struct {
+	gas_pedal      uint16
+	brake_pedal    uint16
+	steering_wheel int16
+	top_speed_kmh  float64
+}
+
+func main() {
+	a_car := car{
+		gas_pedal:      22341,
+		brake_pedal:    0,
+		steering_wheel: 12561,
+		top_speed_kmh:  225.0,
+	}
+
+	fmt.Println(a_car.gas_pedal)
+}
