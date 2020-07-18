a15b11aaeb074baa2dff3d5dd624f6de0cd07553
Philip Afable <philafable@gmail.com>
2020-07-18 11:07:03 -0400

    terraform fmt the main.tf file

--- my-container-test/main.tf
+++ my-container-test/main.tf
@@ -1,30 +1,30 @@
 provider "docker" {
-#   Windows
+  #   Windows
   host = "tcp://localhost:2375"
 
-#   MacOS
-#   host = "unix:///var/run/docker.sock"
+  #   MacOS
+  #   host = "unix:///var/run/docker.sock"
 }
 
 resource "docker_image" "blog_image" {
-    name = terraform.workspace == "prod" ? "ghost:latest" : "wordpress:latest"
+  name = terraform.workspace == "prod" ? "ghost:latest" : "wordpress:latest"
 }
 
 resource "docker_container" "blog_container" {
-    name = "cont-01"
-    image = docker_image.blog_image.latest
-    ports {
-        internal = terraform.workspace == "prod" ? "2368" : "80"
-        external = 80
-    }
-    depends_on = [
-        docker_image.blog_image,
-    ]
+  name  = "cont-01"
+  image = docker_image.blog_image.latest
+  ports {
+    internal = terraform.workspace == "prod" ? "2368" : "80"
+    external = 80
+  }
+  depends_on = [
+    docker_image.blog_image,
+  ]
 }
 output "environment" {
-    value = terraform.workspace
+  value = terraform.workspace
 }
 
 output "docker_image_name" {
-    value = docker_image.blog_image.name
+  value = docker_image.blog_image.name
 }
